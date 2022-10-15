c17ce30c22b01b0ac210598235896a83deb66a8e
vamsi434 <vamsibusa@gmail.com>
2022-10-15 11:04:04 +0530

    tv

--- 01-terraform basics/demo.tf
+++ 01-terraform basics/demo.tf
@@ -1,3 +1,19 @@
-output "sample" {
-    default="hello world"
+variable "sample" {
+    default = "hello world"
+}
+
+output "sample-op" {
+    value = var.sample
+}
+
+output "sample-op1" {
+    value = " value is ${var.sample}"
+}
+
+variable "number" {
+    default = 100
+}
+
+output "number" {
+    value = var.number
 }
