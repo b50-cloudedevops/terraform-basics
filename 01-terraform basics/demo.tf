a73bf4298f5dd11fd12ed0566c45e85b3789be88
vamsi434 <vamsibusa@gmail.com>
2022-10-15 11:41:10 +0530

    map

--- 01-terraform basics/demo.tf
+++ 01-terraform basics/demo.tf
@@ -29,3 +29,11 @@ variable "ex-list" {
 output "ex-list" {
     value = "welcome to ${var.ex-list[0]} devops"
 }
+
+variable "ex-map" {
+    default = {
+        class = "devops"
+        Duration = "85 hours"
+        Trainer = "Manu"
+    }
+}
