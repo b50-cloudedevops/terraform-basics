e0bad706634807104f725ede3ddd9d1dafb5dba6
vamsi434 <vamsibusa@gmail.com>
2022-10-17 12:02:06 +0530

    h

--- 02-ec2/ec2.tf
+++ 02-ec2/ec2.tf
@@ -13,3 +13,27 @@ resource "aws_instance" "demo" {
 output "public_ip" {
     value = aws_instance.demo.public_ip
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssg_sg"
+  }
+}
