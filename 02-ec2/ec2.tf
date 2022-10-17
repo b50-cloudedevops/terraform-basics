c6966639345509445e9d8a96e736a86d640af2ad
vamsi434 <vamsibusa@gmail.com>
2022-10-17 12:06:20 +0530

    n

--- 02-ec2/ec2.tf
+++ 02-ec2/ec2.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "demo" {
     ami = "ami-00ff427d936335825"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     tags = {
       Name = "Vamsi-Ec2-Teraform"
      }
