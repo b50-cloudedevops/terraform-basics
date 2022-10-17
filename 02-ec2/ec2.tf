b8b39dc4fbd7450daf11c606d9013297e3f1b55a
vamsi434 <vamsibusa@gmail.com>
2022-10-17 11:36:20 +0530

    t

--- 02-ec2/ec2.tf
+++ 02-ec2/ec2.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "demo" {
     ami = "ami-00ff427d936335825"
     instance_type = "t2.micro"
     tags = {
-      Name = "Ec2-Teraform"
+      Name = "Vamsi-Ec2-Teraform"
      }
 }
