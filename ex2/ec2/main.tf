a6800617deaefd0b6e601325753ec2ac7aab88c6
vamsi434 <vamsibusa@gmail.com>
2022-10-18 20:15:21 +0530

    tgs

--- ex2/ec2/main.tf
+++ ex2/ec2/main.tf
@@ -1,10 +1,22 @@
 resource "aws_instance" "demo1" {
     ami =  data.aws_ami.my_ami.id
     instance_type = "t2.micro"
-    vpc_security_group_ids =[var.sg]
+    vpc_security_group_ids = [var.sg]
     tags = {
       Name = "Vamsi-Ec2-Teraform"
      }
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+    inline = [
+      "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=frontend -e ENV=dev -e APP_VERSION=0.0.2 roboshop-pull.yml",
+    ]
+  }
 }
 
 variable "sg" {}
--- readme.md
+++ readme.md
@@ -6,3 +6,8 @@ Arguments: Properties that we use to provision the infraa
 Attributes: Properties thaat we use to print the details after the creation of infra like private, public ip
 
 datasource: This is used to fetch the information of the already existing resources on cloud.
+
+Provisioners:
+   a) file provisioner
+   b) remote provisioner
+   c) local-exec provisioner
