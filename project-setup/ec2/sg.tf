9b7620cbe506f6dda008a118a9596f5333f44ee0
vamsi434 <vamsibusa@gmail.com>
2022-10-21 21:47:54 +0530

    thn

---
+++ project-setup/ec2/sg.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_${var.COMPONENT}"
+  description = "Allow SSH inbound traffic"
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "HTTP from internet"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_${var.COMPONENT}"
+  }
+}
+
+output "sg_id" {
+   value = aws_security_group.allow_ssh.id
+}
