25effdf1e6a726437f47ae1f8e0405cf303658b9
vamsi434 <vamsibusa@gmail.com>
2022-10-21 21:50:45 +0530

    es

--- project-setup/ec2/ec2.tf
+++ project-setup/ec2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "spot_worker" {
   ami = data.aws_ami.my_ami.id
   instance_type = "t3.micro"
   wait_for_fulfillment = true
-  vpc_security_group_ids = {aws_security_group.allow_ssh.id}
+  vpc_security_group_ids = [aws_security_group.allows_ssh.id]
   tags = {
     Name = "var.COMPONENT"
   }
