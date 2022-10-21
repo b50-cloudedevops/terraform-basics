f1dfe7f642e417e5844d7ad8cb82f33613358dd3
vamsi434 <vamsibusa@gmail.com>
2022-10-21 21:39:34 +0530

    th

--- project-setup/provider.tf
+++ project-setup/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "b5-tf-state-bucket"
+    key    = "project/env/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
-    reion = "us-east-1"
+    region = "us-east-1"
 }
