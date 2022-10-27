902dcbf6c6bfa96133ec3bbdbfc741af0f152402
vamsi434 <vamsibusa@gmail.com>
2022-10-27 06:19:29 +0530

    ws

--- project-setup/ec2/sg.tf
+++ project-setup/ec2/sg.tf
@@ -10,8 +10,8 @@ resource "aws_security_group" "allow_ssh" {
   }
   ingress {
     description      = "HTTP from internet"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 0
+    to_port          = 0
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
